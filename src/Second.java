b994e7d6117d605deb0fc2a663dabd9b24aab930
Kelechi <nzekelechi@gmail.com>
2024-05-04 11:52:26 +0100

    Initial commit

---
+++ src/Second.java
@@ -0,0 +1,9 @@
+class Second{
+    int x = 3;
+    public static void main(String[] arg){
+        Main myObj = new Main();
+        Second obj = new Second();
+        System.out.println(myObj.x);
+        System.out.println(obj.x);
+    }
+}
